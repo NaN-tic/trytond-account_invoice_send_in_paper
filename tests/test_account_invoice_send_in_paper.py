7889fa53633e771cce62555bcd64fe7e09b467e7
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-12 09:12:18 +0200

    Update tests

--- tests/test_account_invoice_send_in_paper.py
+++ tests/test_account_invoice_send_in_paper.py
@@ -2,9 +2,8 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.exceptions import UserError
 
 
@@ -12,34 +11,32 @@ class TestCase(ModuleTestCase):
     'Test module'
     module = 'account_invoice_send_in_paper'
 
-    def setUp(self):
-        super(TestCase, self).setUp()
-        self.party = POOL.get('party.party')
-
+    @with_transaction()
     def test0010send_in_paper(self):
         'Test send in papaer'
-        with Transaction().start(DB_NAME, USER, context=CONTEXT):
-            self.party.create([{'name': 'Send in paper'}])
-            self.party.create([{
-                        'name': 'Explicit send in paper',
-                        'send_in_paper': True,
-                        }])
-            with self.assertRaises(UserError) as cm:
-                self.party.create([{
-                            'name': 'Not send in paper',
-                            'send_in_paper': False,
-                            }])
-            self.assertEqual(cm.exception.message, (u'Party "Not send in '
-                    'paper" is not marked as send in paper but has no email '
-                    'contact mechanism.'))
-            self.party.create([{
+        pool = Pool()
+        Party = pool.get('party.party')
+        Party.create([{'name': 'Send in paper'}])
+        Party.create([{
+                    'name': 'Explicit send in paper',
+                    'send_in_paper': True,
+                    }])
+        with self.assertRaises(UserError) as cm:
+            Party.create([{
                         'name': 'Not send in paper',
                         'send_in_paper': False,
-                        'contact_mechanisms': [('create', [{
-                                        'type': 'email',
-                                        'value': 'email@example.com',
-                                        }])],
                         }])
+        self.assertEqual(cm.exception.message, (u'Party "Not send in '
+                'paper" is not marked as send in paper but has no email '
+                'contact mechanism.'))
+        Party.create([{
+                    'name': 'Not send in paper',
+                    'send_in_paper': False,
+                    'contact_mechanisms': [('create', [{
+                                    'type': 'email',
+                                    'value': 'email@example.com',
+                                    }])],
+                    }])
 
 
 def suite():
